dcc80f426d066c0de491a401dea3be92157f984b
Diogo Baltazar <ti3@ballke.com.br>
2022-08-03 17:34:56 -0300

    adicionando modo de produção

--- hour_bank/settings.py
+++ hour_bank/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*o^6@ioc=i1okwzlt&$=rse1bd@&_!$q5r71#3c6rp4hc8j6x^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
